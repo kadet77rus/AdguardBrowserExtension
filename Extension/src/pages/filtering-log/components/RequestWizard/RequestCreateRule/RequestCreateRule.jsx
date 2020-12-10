cf70b8d8d451f98ac3e6699c44d7e3da9dd9eb5b
Artem Baskal <a.baskal@adguard.com>
2020-12-10 13:01:29 +0300

    Restore title

--- Extension/src/pages/filtering-log/components/RequestWizard/RequestCreateRule/RequestCreateRule.jsx
+++ Extension/src/pages/filtering-log/components/RequestWizard/RequestCreateRule/RequestCreateRule.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { observer } from 'mobx-react';
-import cn from 'classnames';
 
 import { rootStore } from '../../../stores/RootStore';
 import { RULE_OPTIONS } from '../constants';
@@ -9,8 +8,6 @@ import { reactTranslator } from '../../../../reactCommon/reactTranslator';
 
 import './request-create-rule.pcss';
 import { Icon } from '../../../../common/components/ui/Icon';
-
-const getTitleI18nKey = cn;
 
 const RequestCreateRule = observer(() => {
     const { wizardStore, logStore } = useContext(rootStore);
@@ -129,11 +126,6 @@ const RequestCreateRule = observer(() => {
     const showPatterns = !isElementOrScript && !cookieName;
     const showOptions = !isElementOrScript && !requestRule?.documentLevelRule;
 
-    const titleI18nKey = getTitleI18nKey({
-        filtering_modal_block: wizardStore.requestModalStateEnum.isBlock,
-        filtering_modal_unblock: wizardStore.requestModalStateEnum.isUnblock,
-    });
-
     return (
         <>
             <div className="request-modal__title">
@@ -144,7 +136,7 @@ const RequestCreateRule = observer(() => {
                 >
                     <Icon id="#arrow-left" />
                 </button>
-                <span className="request-modal__header">{reactTranslator.translate(titleI18nKey)}</span>
+                <span className="request-modal__header">{reactTranslator.translate('filtering_modal_add_title')}</span>
             </div>
             <div className="request-modal__content">
                 <div className="request-info request-modal__rule-text">
